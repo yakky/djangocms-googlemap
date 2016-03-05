4f5f00fafc5d530e0a2854e20dc4a372006cab38
Iacopo Spalletti <i.spalletti@nephila.it>
2016-03-05 03:04:40 +0100

    Use python_2_unicode_compatible from Django, not the CMS

--- djangocms_googlemap/models.py
+++ djangocms_googlemap/models.py
@@ -1,8 +1,8 @@
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
 
 from cms.models import CMSPlugin
-from cms.utils.compat.dj import python_2_unicode_compatible
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
 
 
 @python_2_unicode_compatible
